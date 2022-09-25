cf3289be959fe5c311a2f4169714fe2a7323dede
kekrby <kekrby@gmail.com>
2022-09-25 14:35:26 +0300

    apple/t2: update audio files

--- apple/t2/default.nix
+++ apple/t2/default.nix
@@ -3,28 +3,25 @@
 let
   cfg = config.hardware.apple.t2;
 
-  audioFiles = (pkgs.fetchFromGitHub {
+  audioFiles = pkgs.fetchFromGitHub {
     owner = "kekrby";
     repo = "t2-better-audio";
-    rev = "e4e9fba49c6352adeba315c5fc22e2a41b6059b4";
-    sha256 = "sha256-yN2zTms/Vgr9QTtf9ulveOD7dQgHHRbbDs8gbA0AD6M=";
-  });
+    rev = "adbd7640b4055dc79a334ea4d31572e9599b4365";
+    sha256 = "sha256-eoh64WAqhKRJ4m/8WRoX1kn1YJQ9+VicuCkOGk50YuQ=";
+  };
+
+  overrideAudioFiles = package: pluginsPath:
+    package.overrideAttrs (new: old: {
+      preConfigurePhases = old.preConfigurePhases or [] ++ [ "postPatchPhase" ];
+
+      postPatchPhase = ''
+        cp -r ${audioFiles}/files/{profile-sets,paths} ${pluginsPath}/alsa/mixer/
+      '';
+    });
 in
 {
-  nixpkgs = {
-    overlays = [
-      (self: super: {
-        t2-alsa-lib = pkgs.alsa-lib.override {
-          alsa-ucm-conf = pkgs.alsa-ucm-conf.overrideAttrs (new: old: {
-            postInstall = old.postInstall or "" + ''
-              cp -r ${audioFiles}/files/ucm2/* "$out/share/alsa/ucm2/"
-            '';
-          });
-        };
-      })
-    ] ++ map (pkg: self: super: { ${pkg} = super.callPackage ./pkgs/${super.lib.strings.toLower pkg}.nix {}; })
-      [ "t2-linux" "apple-bce" "apple-ibridge" ]; # Some packages depend on others so they have to be imported with order
-  };
+  nixpkgs.overlays = map (pkg: self: super: { ${pkg} = super.callPackage ./pkgs/${super.lib.strings.toLower pkg}.nix {}; })
+    [ "t2-linux" "apple-bce" "apple-ibridge" ]; # Some packages depend on others so they have to be imported with order
 
   # For keyboard and touchbar
   boot.kernelPackages = with pkgs; linuxPackagesFor t2-linux;
@@ -33,22 +30,15 @@ in
 
   # For audio
   boot.kernelParams = [ "pcie_ports=compat" "intel_iommu=on" "iommu=pt" ];
-
-  hardware.pulseaudio.package = (pkgs.pulseaudio.override {
-    alsa-lib = pkgs.t2-alsa-lib;
-  }).overrideAttrs (new: old: {
-    patches = old.patches ++ [
-      (builtins.fetchurl {
-        url = "https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/596.patch";
-        sha256 = "sha256-e64GA+cUjTmVYpnIkhed3E+FzWB0pwNVsMDxp2EvGHo=";
-      })
-    ];
+  services.udev.extraRules = builtins.readFile (pkgs.substitute {
+    src = "${audioFiles}/files/91-audio-custom.rules";
+    replacements = [ "--replace" "/usr/bin/sed" "${pkgs.gnused}/bin/sed" ];
   });
 
+  hardware.pulseaudio.package = overrideAudioFiles pkgs.pulseaudio "src/modules/";
+
   services.pipewire = rec {
-    package = pkgs.pipewire.override {
-      alsa-lib = pkgs.t2-alsa-lib;
-    };
+    package = overrideAudioFiles pkgs.pipewire "spa/plugins/";
 
     wireplumber.package = pkgs.wireplumber.override {
       pipewire = package;
