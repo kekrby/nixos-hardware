1e2e5a5749a1a6709f5858f208f23ce43ce7064e
kekrby <kekrby@gmail.com>
2022-11-26 21:01:33 +0300

    apple/t2: kernel 6.0.10

--- apple/t2/pkgs/linux-t2.nix
+++ apple/t2/pkgs/linux-t2.nix
@@ -2,7 +2,7 @@
 
 buildLinux (args // rec {
   pname = "linux-t2";
-  version = "6.0.9";
+  version = "6.0.10";
   # Snippet from nixpkgs
   modDirVersion = with lib; "${concatStringsSep "." (take 3 (splitVersion "${version}.0"))}-t2";
 
@@ -10,7 +10,7 @@ buildLinux (args // rec {
     owner = "kekrby";
     repo = "linux-t2";
     rev = "v${version}-t2";
-    sha256 = "sha256-gwXRcGckWj+LvWvFpd/u4SN0PHlqIJhvi96tYkJvP3A=";
+    sha256 = "sha256-Vr1CPJpX8YhHArFV3Xr9kQuTgLd1FBEg0W8Lpvi8BNA=";
   };
 
   kernelPatches = [];
